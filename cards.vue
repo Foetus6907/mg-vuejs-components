010dc1503f2e92ecfa33dc9038e124228fd96282
Mohamed Ghoubali <40607644+Foetus6907@users.noreply.github.com>
2018-08-28 18:12:53 +0200

    Create cards.vue

---
+++ cards.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card" style="width: 100%;">
+        <img class="card-img-right" :src="" alt="Card image atl">
+        <div class="card-body">
+            <h5 class="card-title"></h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button href="#" class="btn btn-primary">Detail</button>
+        </div>
+    </div>
+
+</template>
+
+<script>
+ export default {
+     props:{
+         id:Number,
+         element:Object
+     }
+
+ }
+
+</script>
+
+<style scoped>
+
+</style>
