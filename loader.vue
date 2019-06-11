17b252874afa2fc40a441d432a6c9119ca1bb41c
Mohamed Ghoubali <40607644+Foetus6907@users.noreply.github.com>
2019-06-11 09:41:53 +0200

    Loading Component
    Pure HTML CSS loading compo

---
+++ loader.vue
@@ -0,0 +1,72 @@
+<template id=app>
+  <div class="loader">
+    <div class="dots"></div>
+    <div class="dots"></div>
+    <div class="dots"></div>
+    <div class="dots"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    methods: {
+    }
+  };
+</script>
+
+<style scoped>
+  .loader {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+  }
+
+  .loader .dots {
+    position: absolute;
+    padding: 10px;
+    border-radius: 50%;
+    background: lightcoral;
+    -webkit-animation: myani 1s ease-in-out 0s infinite;
+    animation: myani 1s ease-in-out 0s infinite;
+  }
+
+  .loader .dots:nth-child(1) {
+    -webkit-animation-delay: 0s;
+    animation-delay: 0s;
+  }
+
+  .loader .dots:nth-child(2) {
+    -webkit-animation-delay: 0.15s;
+    animation-delay: 0.15s;
+  }
+
+  .loader .dots:nth-child(3) {
+    -webkit-animation-delay: 0.3s;
+    animation-delay: 0.3s;
+  }
+
+  .loader .dots:nth-child(4) {
+    -webkit-animation-delay: 0.45s;
+    animation-delay: 0.45s;
+  }
+
+  @keyframes myani {
+    0% {
+      -webkit-transform: translateX(-100px);
+      transform: translateX(-100px);
+      opacity: 0;
+    }
+
+    50% {
+      opacity: 1;
+    }
+
+    100% {
+      -webkit-transform: translateX(100px);
+      transform: translateX(100px);
+      opacity: 0;
+    }
+</style>
